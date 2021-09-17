6a5c535d00f504d30cde5a6fa0655b5263c21128
juan <juanpablocabana2@gmail.com>
2021-09-17 18:10:30 -0300

    First commit on release_2

--- src/tp/App.java
+++ src/tp/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
 		System.out.println("Esta es una modificación");
+		System.out.println("Release 2");
 	}
 
 }
